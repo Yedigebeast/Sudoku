74701537b30aa1c567ae24b76ed3d5e63780bba9
Yedige Ashirbek <edige11020@gmail.com>
2023-04-29 17:22:45 +0600

    Initial Commit

---
+++ Sudoku/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sudoku
+//
+//  Created by Yedige Ashirbek on 29.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Yedige Ashirbek on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
